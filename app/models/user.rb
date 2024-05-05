83d1bb8f63888a2f1f840d4edf91c117c591478d
norlynj <nacastillo2@up.edu.ph>
2024-05-05 22:53:07 +0800

    appropriate dashboard to admin or staff

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ApplicationRecord
   # Add new attributes to the Devise permitted parameters
   validates :first_name, :last_name, :mobile_number, :gender, :birthday, presence: true
   enum role: { staff: 0, manager: 1 }, _prefix: true
+
+
+  def manager?
+      role == "manager"
+  end
+
+  def staff?
+    role == "staff"
+  end
 end
