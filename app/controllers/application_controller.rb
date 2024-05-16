1bf6ef2d84659ce73544216c22e738b42b232410
norlynj <nacastillo2@up.edu.ph>
2024-05-16 20:47:00 +0800

    Revert "remove unnecessary methods on app controller"
    This reverts commit ea36b21cbc6f2531d6ba28140f4f78585106e6c7.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def after_sign_in_path_for(resource)
+        landing_page_path
+    end
+
+    def configure_permitted_parameters
+        # Permit additional parameters for sign up (registration)
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :mobile_number, :gender, :birthday])
+    end
 end
