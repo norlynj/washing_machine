068c83c72f4b786bd550cb00db32f56d29347b84
barcent <esacompanado@up.edu.ph>
2024-05-18 00:41:58 +0800

    [Transaction] Functional Delete button
    Modal removed for confirmation. Instead, turbo method is used.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -9,7 +9,13 @@ class TransactionsController < ApplicationController
   end
 
   def destroy
-    Transaction.find(params[:id]).destroy
-    redirect_to transaction_crew_index_path, notice: 'Transaction successfully deleted.'
+    @transaction=CustomerTransaction.find(params[:id])
+    @transaction.destroy
+    
+    redirect_to transactions_path, notice: 'Transaction successfully deleted.'
+
+    #redirect_to transactions_path, status: :see_other
+    #Transaction.find(params[:id]).destroy
+    #redirect_to transaction_crew_index_path, notice: 'Transaction successfully deleted.'
   end
 end
