ccb106ca8cec073b7fb50136851506ef50a4592c
Francis Lorenzo <faespina@up.edu.ph>
2024-05-17 17:21:32 +0800

    schedule for staff enabled

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,8 @@
 class LandingController < ApplicationController
     
     def index
+        @weekdays = ['S', 'M', 'T', 'W', 'Th', 'F', 'S']
         render
+        
     end
 end
--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,5 +1,6 @@
 class ScheduleController < ApplicationController
     def index
         @users = User.all
+        @weekdays = ['S', 'M', 'T', 'W', 'Th', 'F', 'S']
     end
 end
