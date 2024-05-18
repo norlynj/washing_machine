3cbe6ceffde175e519b695b3407b33654c0fce6f
Francis Lorenzo <faespina@up.edu.ph>
2024-05-18 13:45:31 +0800

    order model update

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -3,14 +3,21 @@ class AnalyticsController < ApplicationController
  def index
     @customers_by_year = Customer.group_by_year(:created_at).count
     @customers_by_month = Customer.group_by_month(:created_at).count
-
     @years = @customers_by_year.keys.map { |date| date.strftime("%Y") }
     @year_counts = @customers_by_year.values
-
     @months = @customers_by_month.keys.map { |date| date.strftime("%B %Y") }
     @month_counts = @customers_by_month.values
-
     @unique_years = @years.uniq
+
+    @orders_by_year = Order.group_by_year(:created_at).count
+    @orders_by_month = Order.group_by_month(:created_at).count
+    @years_o = @orders_by_year.keys.map { |date| date.strftime("%Y") }
+    @year_counts_o = @orders_by_year.values
+    @months_o = @orders_by_month.keys.map { |date| date.strftime("%B %Y") }
+    @month_counts_o = @orders_by_month.values
+    @unique_years_o = @years_o.uniq
+
+    @orders_today = Order.created_today.count
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :customer
-
+  scope :created_today, -> { where("DATE(created_at) = ?", Date.today) }
   def staff
     User.find(staff_id)
   end
