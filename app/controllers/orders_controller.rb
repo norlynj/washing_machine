b045f9fbd9ee11fcb6300016646000b2b014b0b4
barcent <esacompanado@up.edu.ph>
2024-05-20 19:37:28 +0800

    Functional Order Creation
    - made 3 new html.erb files (see customer->index folder)
    -  cleaned order index by rendering these 3 modals as partials
    - modified order controller so that staff and customer id are passed
    - created a new controller action "set_total" that calculates the total amount to be paid
    - modified order_params controller action to accommodate the fact that this calculation is no longer made in the view file

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,17 +5,18 @@ class OrdersController < ApplicationController
       @orders = Order.all
     end
   
-    def show
-      @user = Order.find(params[:id])
+    def new
+      @order = Order.new
     end
   
     def create
-      @order = Order.new(order_params)
+      @order = Order.new(order_params.merge(staff_id: current_user.id))
   
       if @order.save
         redirect_to orders_path, notice: 'Order was successfully created.'
       else
-        render :new
+        p "#{@order.errors.full_messages}================================================================"
+        redirect_to orders_path, notice: 'Order creation failed .'
       end
     end
   
@@ -25,8 +26,26 @@ class OrdersController < ApplicationController
       @order = Order.find(params[:id])
     end
   
+    def set_total (classification, num_detergents, weight)
+      # Perform calculations
+      base_price = classification == "Bed Linens" ? 180 : 150
+      num_detergents = num_detergents.to_i
+      load = (weight.to_f / 7).ceil
+
+      # Calculate total without additional detergent fee if num_detergents is auto-calculated
+      if num_detergents == load * 3
+        total = load * base_price
+      else
+        detergent_price = 15
+        total = load * base_price + (num_detergents - load * 3) * detergent_price
+      end
+
+      return total.round(2)
+    end
+
     def order_params
-      params.permit(:customer_id, :status, :weight, :payment_method, :classification, :total_amount, :staff_id, :add_instructions)
+      total_amount = set_total(params[:classification], params[:num_detergents], params[:weight])
+      params.permit(:customer_id, :status, :weight, :payment_method, :classification, :staff_id, :add_instructions).merge(total_amount: total_amount)
     end
   end
   
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,9 +2,9 @@ class TransactionsController < ApplicationController
 
   def index
     @transactions = if params[:search]
-      CustomerTransaction.joins(:customer).where("customers.name LIKE ?", "%#{params[:search]}%").distinct
+      Order.joins(:customer).where("customers.name LIKE ?", "%#{params[:search]}%").distinct
     else
-      CustomerTransaction.all
+      Order.all
     end
   end
 
