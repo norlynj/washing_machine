e105966c6e64997a2f1c521aed4888108d8a66fe
barcent <esacompanado@up.edu.ph>
2024-05-22 15:10:27 +0800

    fix order load

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,6 +2,7 @@ class LandingController < ApplicationController
   before_action :authenticate_user!
 
   def index
+        @orders_today = Order.created_today.count
         @weekdays = ['S', 'M', 'T', 'W', 'Th', 'F', 'S']
         render
     end
