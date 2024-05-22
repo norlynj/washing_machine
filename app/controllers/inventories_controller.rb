2ec8b99f0a606e4ea97d1c6b473aeb4d96d0b7e8
barcent <esacompanado@up.edu.ph>
2024-05-22 08:47:35 +0800

    [Inventory]
    IN: add item

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,9 +1,23 @@
 class InventoriesController < ApplicationController
   before_action :authenticate_user!
-
+  
   def index
     @inventories = Inventory.all
   end
-
-
+  
+  def create
+    @inventory = Inventory.new(inventory_params)
+    
+    if @inventory.save
+      redirect_to inventories_path, notice: "Item was successfully added"
+    else
+      redirect_to inventories_path, alert: "Item creation failed"
+    end
+  end
+  
+  private
+  
+  def inventory_params
+    params.permit(:quantity, :name)
+  end
 end
