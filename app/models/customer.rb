bd064212ba0f0259eb6a93ed43c874ecfafe05d9
norlynj <nacastillo2@up.edu.ph>
2024-05-22 14:56:40 +0800

    add validation for contact info

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
   has_many :orders
+  validates :contact_info, format: { with: /\A\d{11}\z/, message: "must be 11 digits" }
   scope :created_today, -> { where("DATE(created_at) = ?", Date.today) }
 end
